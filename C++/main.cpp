dada9514f01999b4268c8c6392479995d211f369
zerossk <a.k.jack.ssk@gmail.com>
2019-06-23 01:31:05 +0900

    Add C++

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <map>
 #include <unordered_map>
+#include <random>
+#include <cstring>
 
 using namespace std;
 
 int main(){
-	unordered_map<string, int> island = {
+	unordered_map<string, int> Island = {
 		{"軍艦島", 6},
 		{"小豆島", 6},
 		{"種子島", 6},
@@ -13,9 +14,35 @@ int main(){
 		{"屋久島", 6},
 	};
 
-	for (auto i = island.begin(); i != island.end(); ++i){
-		cout << i[0] << "\n";
-		cout << i->first << i->second-1 << "\n";
+	auto island = unordered_map<string, int>(Island);
+	random_device rand;
+
+	string fmem = "\nエンターキーを押してください\n reset でリセット\n";
+	string smem = "に座りましょう 空き";
+
+	for (;;){
+		if (island.size() == 0) {
+			printf("\n座席がゼロになったためリセットしました\n");
+			island = unordered_map<string, int>(Island);
+			continue;
+		}
+		auto item = island.begin();
+		char rst[64];
+
+		cout << fmem << endl;
+
+		if (strcmp(fgets(rst, 64, stdin), "reset\n") != 0){
+			advance(item, rand() % island.size());
+			item->second -= 1;
+
+			cout << item->first << smem << item->second << endl;
+			if (item->second == 0) {
+				island.erase(item->first);
+			}
+		}else{
+			printf("\nリセットしました\n");
+			island = unordered_map<string, int>(Island);
+		}
 	}
 
 	return 0;
